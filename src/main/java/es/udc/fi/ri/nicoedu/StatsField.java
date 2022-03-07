6ff6c2d2911fb1a7fe3930eba324466298bdb62d
fraguela <eduardoperezfraguela@gmail.com>
2022-03-07 13:01:43 +0100

    Comienzo StatsField

--- src/main/java/es/udc/fi/ri/nicoedu/StatsField.java
+++ src/main/java/es/udc/fi/ri/nicoedu/StatsField.java
@@ -1,4 +1,22 @@
 package es.udc.fi.ri.nicoedu;
 
+import org.apache.lucene.store.Directory;
+
 public class StatsField {
+
+    private StatsField(){    
+    }
+
+    public static void main(String[] args) {
+
+        String usage = "java StatsField <-index INDEX_PATH> [-field FIELD]";
+
+        Directory dir = null;
+        String indexPath = null;
+        String fieldString = null;
+        
+    }
+    //-index
+    //-field
+
 }
