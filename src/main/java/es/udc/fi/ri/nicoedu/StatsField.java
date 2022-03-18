7ddbd7121cc7007cb8230463e2c1fd5ab85d1eaa
fraguela <eduardoperezfraguela@gmail.com>
2022-03-18 16:03:10 +0100

    StatsField acabado

--- src/main/java/es/udc/fi/ri/nicoedu/StatsField.java
+++ src/main/java/es/udc/fi/ri/nicoedu/StatsField.java
@@ -1,7 +1,6 @@
 package es.udc.fi.ri.nicoedu;
 
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.*;
 import org.apache.lucene.search.CollectionStatistics;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.Directory;
@@ -44,20 +43,35 @@ public class StatsField {
             dir = FSDirectory.open(Paths.get(indexPath));  //abre los archivos de indice del sistema de archivos
             indexReader = DirectoryReader.open(dir);    //lee el indice  en un directorio
             indexSearcher = new IndexSearcher(indexReader);
-        }catch (IOException e) {
-            System.out.println("Exception:" + e);
+        } catch (CorruptIndexException e){
+            System.out.println("Exception: " + e);
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Exception: " + e);
             e.printStackTrace();
         }
 
         if (fieldString != null){
             try{
                 cs = indexSearcher.collectionStatistics(fieldString);
+                if(cs == null){
+                    System.err.println("Campo " + fieldString + " no existe o no tiene terminos");
+                }
+                else{
+                    System.out.println(cs.toString());
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        else{
+            final FieldInfos fieldInfos = FieldInfos.getMergedFieldInfos(indexReader);
+            for(final FieldInfo fieldInfo : fieldInfos){
+                try {
+                    cs = indexSearcher.collectionStatistics(fieldInfo.name);
+                    System.out.println(cs.toString() + "\n");
+                } catch (Exception ignore) {}
+            }
+        }
     }
-    //-index
-    //-field
-
 }
